146d68121d20ac2b9c549a21a05db9b4e8773bc6
Aytunç Demir <112761562+aytuncdemr@users.noreply.github.com>
2025-03-04 15:06:04 +0300

    Update Cat.java

--- module1/src/tr/edu/maltepe/oop/Cat.java
+++ module1/src/tr/edu/maltepe/oop/Cat.java
@@ -1,35 +1,24 @@
 class Cat {
     private String name;
     private int age;
-
-    // Constructor
     public Cat(String name, int age) {
         this.name = name;
         this.age = age;
     }
 
-    // Getter for name
     public String getName() {
         return name;
     }
-
-    // Setter for name
     public void setName(String name) {
         this.name = name;
     }
-
-    // Getter for age
     public int getAge() {
         return age;
     }
-
-    // Setter for age
     public void setAge(int age) {
         this.age = age;
     }
-
-    // Meow method
     public void meow() {
-        System.out.println(name + " says: Meow!");
+        System.out.println("Meow!");
     }
 }
