16566eedea4ba48f79da725e0bdf1e3d21efcdd1
Aytunç Demir <112761562+aytuncdemr@users.noreply.github.com>
2025-03-04 15:36:54 +0300

    Update Cat.java

--- module1/src/tr/edu/maltepe/oop/Cat.java
+++ module1/src/tr/edu/maltepe/oop/Cat.java
@@ -1,26 +1,31 @@
 package tr.edu.maltepe.oop;
 
 class Cat {
-    private String name;
-    private int age;
-    public Cat(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
+   private String name;
+   private int age;
 
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public int getAge() {
-        return age;
-    }
-    public void setAge(int age) {
-        this.age = age;
-    }
-    public void meow() {
-        System.out.println("Meow!");
-    }
+   public Cat(String var1, int var2) {
+      this.name = var1;
+      this.age = var2;
+   }
+
+   public String getName() {
+      return this.name;
+   }
+
+   public void setName(String var1) {
+      this.name = var1;
+   }
+
+   public int getAge() {
+      return this.age;
+   }
+
+   public void setAge(int var1) {
+      this.age = var1;
+   }
+
+   public void meow() {
+      System.out.println("Meow!");
+   }
 }
