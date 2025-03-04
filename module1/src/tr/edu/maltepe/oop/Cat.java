ab754ca2bfcc8deb013140c066c4386b3d27adc8
Aytunç Demir <112761562+aytuncdemr@users.noreply.github.com>
2025-03-04 16:06:38 +0300

    Update Cat.java

--- module1/src/tr/edu/maltepe/oop/Cat.java
+++ module1/src/tr/edu/maltepe/oop/Cat.java
@@ -1,6 +1,12 @@
-package tr.edu.maltepe.oop;
+import java.io.File;
+import java.io.IOException;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 
-class Cat {
+public class Cat {
    private String name;
    private int age;
 
@@ -26,6 +32,27 @@ class Cat {
    }
 
    public void meow() {
-      System.out.println("Meow!");
+      System.out.println(this.name + " says: Meow!");
+      this.playMeowSound("meow.wav");
+   }
+
+   private void playMeowSound(String var1) {
+      try {
+         File var2 = new File(var1);
+         AudioInputStream var3 = AudioSystem.getAudioInputStream(var2);
+         Clip var4 = AudioSystem.getClip();
+         var4.open(var3);
+         var4.start();
+      } catch (IOException | LineUnavailableException | UnsupportedAudioFileException var5) {
+         System.err.println("Error playing sound: " + var5.getMessage());
+      }
+
+   }
+
+   public static void main(String[] var0) {
+      Cat var1 = new Cat("Whiskers", 3);
+      Cat var2 = new Cat("Mittens", 2);
+      var1.meow();
+      var2.meow();
    }
 }
