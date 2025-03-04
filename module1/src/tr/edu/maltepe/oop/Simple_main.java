b874ae758858f5dbff418c733a1556c5d27e62a8
Aytunç Demir <112761562+aytuncdemr@users.noreply.github.com>
2025-03-04 15:09:24 +0300

    Update Simple_main.java

--- module1/src/tr/edu/maltepe/oop/Simple_main.java
+++ module1/src/tr/edu/maltepe/oop/Simple_main.java
@@ -1,17 +1,12 @@
 package tr.edu.maltepe.oop;
 
-
-// Main class to create objects and call the meow method
 public class Simple_main {
     public static void main(String[] args) {
-        // Creating two Cat objects
-        Cat cat1 = new Cat("Whiskers", 3);
-        Cat cat2 = new Cat("Mittens", 5);
+        
+        Cat cat1 = new Cat("Aytunc", 3);
+        Cat cat2 = new Cat("Kemal", 5);
 
-        // Calling the meow method
         cat1.meow();
         cat2.meow();
     }
 }
-
-
